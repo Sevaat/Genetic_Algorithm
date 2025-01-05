d2218aa6f3e6ddd9d50bd71c51d32d88493f1473
tkachenko <sevaatmail@gmail.com>
2025-01-05 23:03:58 +0500

    Юнит-тесты
    Тесты для проверки чтения файла. Обработаны параметры генетического алгоритма.

--- src/tests/utils_tests/FileManagerClass_tests.py
+++ src/tests/utils_tests/FileManagerClass_tests.py
@@ -7,7 +7,6 @@ from src.variation_modules.TargetFunctionClass import TargetFunction
 from src.variation_modules.PopulationClass import Population
 from src.variation_modules.ReplacementClass import Replacement
 
-
 filepath_1 = "../data_test/Data_Test_1.txt"
 filepath_2 = "../data_test/Data_Test_2.txt"
 filepath_3 = "../data_test/Data_Test_3.txt"
@@ -15,7 +14,7 @@ filepath_3 = "../data_test/Data_Test_3.txt"
 
 class FileManagerClassTests(unittest.TestCase):
 
-    def  test_get_genetic_algorithm_1(self):
+    def test_get_genetic_algorithm_1(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -23,7 +22,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, "<class 'src.model.genetic_algorithm.GeneticAlgorithmClass.GeneticAlgorithm'>")
 
-    def  test_get_genetic_algorithm_2(self):
+    def test_get_genetic_algorithm_2(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -31,7 +30,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Selection.standart_selection)
 
-    def  test_get_genetic_algorithm_3(self):
+    def test_get_genetic_algorithm_3(self):
         filepath = filepath_2
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -39,7 +38,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Selection.stochastic_universal_sampling)
 
-    def  test_get_genetic_algorithm_4(self):
+    def test_get_genetic_algorithm_4(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -47,7 +46,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Purpose.sort_by_less)
 
-    def  test_get_genetic_algorithm_5(self):
+    def test_get_genetic_algorithm_5(self):
         filepath = filepath_2
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -55,7 +54,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Purpose.sort_by_more)
 
-    def  test_get_genetic_algorithm_6(self):
+    def test_get_genetic_algorithm_6(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -63,7 +62,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Recombination.point_crossing)
 
-    def  test_get_genetic_algorithm_7(self):
+    def test_get_genetic_algorithm_7(self):
         filepath = filepath_2
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -71,7 +70,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Recombination.segmental_crossing)
 
-    def  test_get_genetic_algorithm_8(self):
+    def test_get_genetic_algorithm_8(self):
         filepath = filepath_3
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -79,7 +78,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Recombination.even_crossing)
 
-    def  test_get_genetic_algorithm_9(self):
+    def test_get_genetic_algorithm_9(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -87,7 +86,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, TargetFunction.get_result_test_function)
 
-    def  test_get_genetic_algorithm_10(self):
+    def test_get_genetic_algorithm_10(self):
         filepath = filepath_2
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -95,7 +94,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, TargetFunction.get_result_user_defined_function)
 
-    def  test_get_genetic_algorithm_11(self):
+    def test_get_genetic_algorithm_11(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -103,7 +102,7 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Population.get_new_random_population)
 
-    def  test_get_genetic_algorithm_12(self):
+    def test_get_genetic_algorithm_12(self):
         filepath = filepath_1
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
@@ -111,13 +110,78 @@ class FileManagerClassTests(unittest.TestCase):
 
         self.assertEqual(result, Replacement.elite)
 
-    def  test_get_genetic_algorithm_13(self):
+    def test_get_genetic_algorithm_13(self):
         filepath = filepath_2
         ga = fmc.FileManager.get_genetic_algorithm(filepath)
 
         result = ga.replacement
 
         self.assertEqual(result, Replacement.simple_cut)
+
+    def test_get_parameters_1(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.number_of_individuals
+
+        self.assertEqual(result, 100)
+
+    def test_get_parameters_2(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.proportion_of_elite_individuals
+
+        self.assertEqual(result, 0.2)
+
+    def test_get_parameters_3(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.number_of_eras
+
+        self.assertEqual(result, 10)
+
+    def test_get_parameters_4(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.mutation_probability
+
+        self.assertEqual(result, 0.02)
+
+    def test_get_parameters_5(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.change_counter
+
+        self.assertEqual(result, 10)
+
+    def test_get_parameters_6(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.number_of_results
+
+        self.assertEqual(result, 5)
+
+    def test_get_parameters_7(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = parameters.recombination_point_count
+
+        self.assertEqual(result, 1)
+
+    def test_get_parameters_8(self):
+        filepath = filepath_1
+        parameters = fmc.FileManager.get_parameters(filepath)
+
+        result = str(parameters.gene_sets)
+
+        self.assertEqual(result, str([['0', '1', '2', '3', '4'], ['5.0', '6.0', '7.0', '8.0', '9.0'],
+                                      ['10', '11', '12', '13', '14']]))
 
 
 if __name__ == '__main__':
