80738ed5d200fd1bb1a28c4dfb00032af8642914
tkachenko <sevaatmail@gmail.com>
2025-01-12 23:34:21 +0500

    Изменение
    Удалены устаревшие методы.

--- src/variation_modules/RecombinationClass.py
+++ src/variation_modules/RecombinationClass.py
@@ -72,45 +72,6 @@ class Recombination(ABC):
         return childrens
 
     @staticmethod
-    def single_point_crossing(parents: [[Individual]]) -> [Individual]:
-        """
-        Одноточечное скрещивание (случайная точка для обмена частью генотипа особей)
-        :param parents: список родителей
-        :return: список детей
-        """
-        childrens = []
-        for twos in parents:
-            point = random.randint(1, len(twos[0].code) - 2)
-            children = Individual.new_individual_by_code(twos[0].code[0:point] + twos[1].code[point:])
-            childrens.append(children)
-            children = Individual.new_individual_by_code(twos[1].code[0:point] + twos[0].code[point:])
-            childrens.append(children)
-        return childrens
-
-    @staticmethod
-    def two_point_crossing(parents: [[Individual]]) -> [Individual]:
-        """
-        Двухточечное скрещивание (случайные точки (2) для обмена частью генотипа особей)
-        :param parents: список родителей
-        :return: список детей
-        """
-        childrens = []
-        for fours in parents:
-            point_1 = random.randint(1, len(fours[0].code) - 2)
-            point_2 = random.randint(1, len(fours[0].code) - 2)
-            points = sorted([point_1, point_2])
-            children = []
-            for i, p1 in enumerate(fours):
-                for p2 in fours[i + 1:]:
-                    code = p1.code[0:points[0]] + p2.code[points[0]:points[1]] + p1.code[points[1]:]
-                    children = Individual.new_individual_by_code(code)
-                    childrens.append(children)
-                    code = p2.code[0:points[0]] + p1.code[points[0]:points[1]] + p2.code[points[1]:]
-                    children = Individual.new_individual_by_code(code)
-                    childrens.append(children)
-        return childrens
-
-    @staticmethod
     def get_points(length: int) -> [int]:
         """
         Получение точек для рекомбинации
