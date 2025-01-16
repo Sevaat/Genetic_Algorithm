7d92c5b2d9981f04bee686beff91b5ecb50296a6
tkachenko <sevaatmail@gmail.com>
2025-01-16 09:20:17 +0500

    Изменение
    Создание начальной популяции с заданным количеством повторяющихся особей.

--- src/variation_modules/PopulationClass.py
+++ src/variation_modules/PopulationClass.py
@@ -1,18 +1,20 @@
 from abc import ABC
 from src.model.IndividualClass import *
+from src.utils.DuplicateCheckClass import DuplicateCheck
 
 
 class Population(ABC):
     @staticmethod
     def get_new_random_population() -> [Individual]:
         """
-        Создание новой случайной популяции
+        Создание новой случайной популяции без повторений
         :return: список особей
         """
         population = []
         while len(population) < GV.PARAMETERS.number_of_individuals:
             individual = IndividualFactory.new_random_individual()
-            population.append(individual)
+            if DuplicateCheck.individual_addition(population, individual):
+                population.append(individual)
         return population
 
 
