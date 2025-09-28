80c45b7abb55a800a03e4a02bd992232b0ec45ec
vsevolod tkachenko <sevaatmail@gmail.com>
2025-09-28 20:15:31 +0500

    Расчет целевой функции

--- src/classical_genetic_algorithm/model/cga_target_function.py
+++ src/classical_genetic_algorithm/model/cga_target_function.py
@@ -1,27 +1,8 @@
 from abc import ABC
-from src.model.IndividualClass import *
-import src.utils.GlobalVariables as GV
-import sys
+from src.classical_genetic_algorithm.model.cga_individual import Individual
 
 
 class TargetFunction(ABC):
-    @staticmethod
-    def get_result_test_function(individuals: [Individual]) -> [Individual]:
-        """
-        Тестовая функция суммы значений хромосом
-        :param individuals: список особей
-        :return: список особей без некорректных (значение хромосомы выходит за допустимые границы)
-        """
-        new_individuals = []
-        for ind in individuals:
-            if ind.overstepping():
-                rank = 0
-                for chromosome in ind.transcript_individual():
-                    rank += float(chromosome)
-                ind.rank = rank
-                new_individuals.append(ind)
-        return new_individuals
-
     @staticmethod
     def get_result_user_defined_function(individuals: [Individual]) -> [Individual]:
         """
@@ -29,19 +10,17 @@ class TargetFunction(ABC):
         :param individuals: список особей
         :return: список особей без некорректных (значение хромосомы выходит за допустимые границы)
         """
+        from src.classical_genetic_algorithm.options_ga.cga_config import Config
+        config = Config()
         new_individuals = []
         for ind in individuals:
             if ind.overstepping():
                 individual_parameters = ind.transcript_individual()
                 try:
-                    ind.rank = GV.GENETIC_ALGORITHM.user_function_reference(individual_parameters)
-                except:
+                    ind.rank = config.settings.target_function(individual_parameters)
+                except Exception as e:
+                    print(f'Ошибка: {e}')
                     print('Введена некорректная функция или функция принимает некорректные аргументы.')
                     print('Проверьте правильность введенной информации и повторите попытку.')
-                    sys.exit()
                 new_individuals.append(ind)
         return new_individuals
-
-
-if __name__ == '__main__':
-    pass
