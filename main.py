35c383fa6bee1253df6105145f07c0bf072cff3a
vsevolod tkachenko <sevaatmail@gmail.com>
2025-09-28 20:00:13 +0500

    Редактирование организации проекта

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from typing import Callable
-
-from src.model.config import Config
-from src.utils.file_manager import FileManager
-from src.GeneticAlgorithm import genetic_algorithm
-
-
-def main_ga(user_function: Callable):
-    setting, parameters = FileManager.open_file()
-    config = Config(setting, parameters, user_function)
-    del setting, parameters
-    result = genetic_algorithm()
-    FileManager.save_file(result)
+def main():
+    pass
