314e54f2ac07a5039d63874bbf086acb1844fea7
vsevolod tkachenko <sevaatmail@gmail.com>
2025-09-28 20:15:42 +0500

    Выбор родителей

--- src/classical_genetic_algorithm/model/cga_parent_selection.py
+++ src/classical_genetic_algorithm/model/cga_parent_selection.py
@@ -1,13 +1,13 @@
 from abc import ABC
 import random
-from src.model.IndividualClass import Individual
+
+from src.classical_genetic_algorithm.model.cga_individual import Individual
 from src.variation_modules.PurposeClass import Purpose
-import src.utils.GlobalVariables as GV
 
 
 class Selection(ABC):
     @staticmethod
-    def standart_selection(individuals: [Individual]) -> [Individual]:
+    def standard_selection(individuals: [Individual]) -> list[tuple[Individual, Individual]]:
         """
         Выбор родителей в соответствии с весом (значение ЦФ)
         :param individuals: список взвешенных (со значением ЦФ) особей
@@ -18,13 +18,13 @@ class Selection(ABC):
         while len(parents) < int(len(individuals)):
             p_1, p_2 = random.choices(individuals, weights=weights, k=2)
             if p_1.code != p_2.code:
-                parents.append([p_1, p_2])
+                parents.append((p_1, p_2))
             else:
                 continue
         return parents
 
     @staticmethod
-    def stochastic_universal_sampling(individuals: [Individual]) -> [Individual]:
+    def stochastic_universal_sampling(individuals: [Individual]) -> list[tuple[Individual, Individual]]:
         """
         Выбор родителей с помощью стохастической универсальной выборки (1 - случайно; 3 - со смещением в четверть)
         :param individuals: список взвешенных (со значением ЦФ) особей
@@ -55,7 +55,7 @@ class Selection(ABC):
                     break
             for i, p1 in enumerate(new_parents):
                 for p2 in new_parents[i + 1:]:
-                    parents.append([p1, p2])
+                    parents.append((p1, p2))
         return parents
 
     @staticmethod
@@ -65,14 +65,12 @@ class Selection(ABC):
         :param individuals: список особей (потенциальных родителей)
         :return: список относительных весов пропорциональных значению ЦФ
         """
+        from src.classical_genetic_algorithm.options_ga.cga_config import Config
+        config = Config()
         total_weight = sum([p.rank for p in individuals])
-        if GV.GENETIC_ALGORITHM.purpose == Purpose.sort_by_less:
+        if config.settings.purpose == Purpose.sort_by_less:
             weights = [p.rank / total_weight for p in individuals]
             return weights
-        if GV.GENETIC_ALGORITHM.purpose == Purpose.sort_by_more:
+        if config.settings.purpose == Purpose.sort_by_more:
             weights = [(total_weight - p.rank) / total_weight for p in individuals]
             return weights
-
-
-if __name__ == '__main__':
-    pass
